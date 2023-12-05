77c2affbd1fd2bc0b42aebf4c935e8007e4fac1a
Bar Barina <itsbarbarina@gmail.com>
2023-12-05 12:45:32 +0200

    rendered home in app js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Home from "./views/Home";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
+  // const Stack = createNativeStackNavigator();
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Home/>
+      {/* <NavigationContainer>
+        <Stack.Navigator> */}
+          {/* <Stack.Screen name="Home" component={Home} /> */}
+        {/* </Stack.Navigator>
+      </NavigationContainer> */}
     </View>
   );
 }
